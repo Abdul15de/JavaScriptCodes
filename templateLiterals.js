a47165715965de4fab2249d5e3f6c52a9dbc5ce6
Abdul15de <abdun9de.15@gmail.com>
2024-02-11 17:13:35 +0530

    Javascript basics

---
+++ templateLiterals.js
@@ -0,0 +1,9 @@
+// Template Literals - A way to add expressions in the strings 
+// note ALso called String Interpolation
+let a=10;
+let b=20;
+
+let specialString = `First number is ${a+b} Second nmber is ${b}`;
+console.log(specialString);
+console.log(typeof(specialString));//String
+
