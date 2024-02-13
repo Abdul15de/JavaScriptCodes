3f2f07cf0f8b20acb2dce7742c0ade26eef9e29e
Abdul15de <abdun9de.15@gmail.com>
2024-02-13 12:17:12 +0530

    Java Script basics

---
+++ pracString.js
@@ -0,0 +1,5 @@
+// Q-toinput a string and produce username 
+// "Abbsid" username="@Abbsid6" 6 is length of string
+let str = prompt("Enter your Name");
+let userName = "@" + str + str.length;
+console.log(userName);
---
+++ string.js
@@ -0,0 +1,12 @@
+let str = "Apna college";
+console.log(str[0]); //To access character of string
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(3,7));
+console.log(str.concat(" Abdullah")); //This can also be done using +- str+"Abdullah"
+console.log(str.replace("co","abd")); //Replaces on part of string eith anathor string
+console.log(str.charAt(6));
+//original string is immutable it can not be changed
+
+
